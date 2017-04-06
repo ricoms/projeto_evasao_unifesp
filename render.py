7ed7096ea24cb7201240ef5fecee0ec72b44b434
ricoms <ricardosavii@gmail.com>
2017-04-06 19:44:17 -0300

    correção de 'Previsão de desistentes.ipynb'

--- render.py
+++ render.py
@@ -46,3 +46,54 @@ def plot_calibration_curve(classificadores, X_train, y_train, X_test, y_test, po
     #ax2.legend(loc="upper center", ncol=2)
 
     plt.tight_layout()
+
+from time import time
+from sklearn.metrics import f1_score
+
+def train_classifier(clf, X_train, y_train, clf_params):
+    ''' Fits a classifier to the training data. '''
+    
+    # Start the clock, train the classifier, then stop the clock
+    if False:
+        start = time()
+        svr = GridSearchCV(clf, clf_params)
+        svr.fit(X_train, y_train[0].values)
+        end = time()
+        clf = svr.best_estimator_
+    else:
+        start = time()
+        clf.fit(X_train, y_train)
+        end = time()
+    
+    # Print the results
+    print ("Trained model in {:.4f} seconds".format(end - start))
+
+    
+def predict_labels(clf, features, target):
+    ''' Makes predictions using a fit classifier based on F1 score. '''
+    
+    # Start the clock, make predictions, then stop the clock
+    start = time()
+    y_pred = clf.predict(features)
+    end = time()
+    
+    # Print and return results
+    print ("Made predictions in {:.4f} seconds.".format(end - start))
+    if True:
+        return f1_score(target.values, y_pred, pos_label='SIM')
+    else:
+        return f1_score(target.values, y_pred, pos_label=54)
+
+
+def train_predict(clf, X_train, y_train, X_test, y_test, clf_params):
+    ''' Train and predict using a classifer based on F1 score. '''
+    
+    # Indicate the classifier and the training set size
+    print ("Training a {} using a training set size of {}. . .".format(clf.__class__.__name__, len(X_train)))
+    
+    # Train the classifier
+    train_classifier(clf, X_train, y_train, clf_params)
+    
+    # Print the results of prediction for both training and testing
+    print ("F1 score for training set: {:.4f}.".format(predict_labels(clf, X_train, y_train)))
+    print ("F1 score for test set: {:.4f}.".format(predict_labels(clf, X_test, y_test)))
