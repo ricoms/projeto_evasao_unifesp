8f0dab9d965f121a54204be2c47d5c14b529a726
ricoms <ricardosavii@gmail.com>
2017-04-06 20:47:08 -0300

    inclusão de visualização no 'Análiseda base.ipynb'

--- render.py
+++ render.py
@@ -47,53 +47,113 @@ def plot_calibration_curve(classificadores, X_train, y_train, X_test, y_test, po
 
     plt.tight_layout()
 
-from time import time
-from sklearn.metrics import f1_score
+import matplotlib.cm as cm
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import pca
 
-def train_classifier(clf, X_train, y_train, clf_params):
-    ''' Fits a classifier to the training data. '''
-    
-    # Start the clock, train the classifier, then stop the clock
-    if False:
-        start = time()
-        svr = GridSearchCV(clf, clf_params)
-        svr.fit(X_train, y_train[0].values)
-        end = time()
-        clf = svr.best_estimator_
-    else:
-        start = time()
-        clf.fit(X_train, y_train)
-        end = time()
-    
-    # Print the results
-    print ("Trained model in {:.4f} seconds".format(end - start))
+def pca_results(good_data, pca):
+	'''
+	Create a DataFrame of the PCA results
+	Includes dimension feature weights and explained variance
+	Visualizes the PCA results
+	'''
 
-    
-def predict_labels(clf, features, target):
-    ''' Makes predictions using a fit classifier based on F1 score. '''
-    
-    # Start the clock, make predictions, then stop the clock
-    start = time()
-    y_pred = clf.predict(features)
-    end = time()
-    
-    # Print and return results
-    print ("Made predictions in {:.4f} seconds.".format(end - start))
-    if True:
-        return f1_score(target.values, y_pred, pos_label='SIM')
-    else:
-        return f1_score(target.values, y_pred, pos_label=54)
+	# Dimension indexing
+	dimensions = dimensions = ['Dimension {}'.format(i) for i in range(1,len(pca.components_)+1)]
+
+	# PCA components
+	components = pd.DataFrame(np.round(pca.components_, 4), columns = good_data.keys())
+	components.index = dimensions
+
+	# PCA explained variance
+	ratios = pca.explained_variance_ratio_.reshape(len(pca.components_), 1)
+	variance_ratios = pd.DataFrame(np.round(ratios, 4), columns = ['Explained Variance'])
+	variance_ratios.index = dimensions
+
+	# Create a bar plot visualization
+	fig, ax = plt.subplots(figsize = (14,8))
+
+	# Plot the feature weights as a function of the components
+	components.plot(ax = ax, kind = 'bar');
+	ax.set_ylabel("Feature Weights")
+	ax.set_xticklabels(dimensions, rotation=0)
+	ax.legend_.remove()
+
+	# Display the explained variance ratios
+	for i, ev in enumerate(pca.explained_variance_ratio_):
+		ax.text(i-0.40, ax.get_ylim()[1] + 0.05, "Explained Variance\n          %.4f"%(ev))
+
+	# Return a concatenated DataFrame
+	return pd.concat([variance_ratios, components], axis = 1)
+
+def cluster_results(reduced_data, preds, centers):
+	'''
+	Visualizes the PCA-reduced cluster data in two dimensions
+	Adds cues for cluster centers and student-selected sample data
+	'''
+
+	predictions = pd.DataFrame(preds, columns = ['Cluster'])
+	plot_data = pd.concat([predictions, reduced_data], axis = 1)
+
+	# Generate the cluster plot
+	fig, ax = plt.subplots(figsize = (14,8))
+
+	# Color map
+	cmap = cm.get_cmap('gist_rainbow')
+
+	# Color the points based on assigned cluster
+	for i, cluster in plot_data.groupby('Cluster'):   
+	    cluster.plot(ax = ax, kind = 'scatter', x = 'Dimension 1', y = 'Dimension 2', \
+	                 color = cmap((i)*1.0/(len(centers)-1)), label = 'Cluster %i'%(i), s=30);
+
+	# Plot centers with indicators
+	for i, c in enumerate(centers):
+	    ax.scatter(x = c[0], y = c[1], color = 'white', edgecolors = 'black', \
+	               alpha = 1, linewidth = 2, marker = 'o', s=200);
+	    ax.scatter(x = c[0], y = c[1], marker='$%d$'%(i), alpha = 1, s=100);
+
+	# Plot transformed sample points 
+	#ax.scatter(x = pca_samples[:,0], y = pca_samples[:,1], \
+	           #s = 150, linewidth = 4, color = 'black', marker = 'x');
+
+	# Set plot title
+	ax.set_title("Cluster Learning on PCA-Reduced Data - Centroids Marked by Number\nTransformed Sample Data Marked by Black Cross");
 
 
-def train_predict(clf, X_train, y_train, X_test, y_test, clf_params):
-    ''' Train and predict using a classifer based on F1 score. '''
-    
-    # Indicate the classifier and the training set size
-    print ("Training a {} using a training set size of {}. . .".format(clf.__class__.__name__, len(X_train)))
-    
-    # Train the classifier
-    train_classifier(clf, X_train, y_train, clf_params)
-    
-    # Print the results of prediction for both training and testing
-    print ("F1 score for training set: {:.4f}.".format(predict_labels(clf, X_train, y_train)))
-    print ("F1 score for test set: {:.4f}.".format(predict_labels(clf, X_test, y_test)))
+def channel_results(reduced_data, outliers, pca_samples):
+	'''
+	Visualizes the PCA-reduced cluster data in two dimensions using the full dataset
+	Data is labeled by "Channel" and cues added for student-selected sample data
+	'''
+
+	# Check that the dataset is loadable
+	try:
+	    full_data = pd.read_csv("customers.csv")
+	except:
+	    print ("Dataset could not be loaded. Is the file missing?")
+	    return False
+
+	# Create the Channel DataFrame
+	channel = pd.DataFrame(full_data['Channel'], columns = ['Channel'])
+	channel = channel.drop(channel.index[outliers]).reset_index(drop = True)
+	labeled = pd.concat([reduced_data, channel], axis = 1)
+	
+	# Generate the cluster plot
+	fig, ax = plt.subplots(figsize = (14,8))
+
+	# Color map
+	cmap = cm.get_cmap('gist_rainbow')
+
+	# Color the points based on assigned Channel
+	labels = ['Hotel/Restaurant/Cafe', 'Retailer']
+	grouped = labeled.groupby('Channel')
+	for i, channel in grouped:   
+	    channel.plot(ax = ax, kind = 'scatter', x = 'Dimension 1', y = 'Dimension 2', \
+	                 color = cmap((i-1)*1.0/2), label = labels[i-1], s=30);
+	    
+	# Plot transformed sample points   
+	for i, sample in enumerate(pca_samples):
+		ax.scatter(x = sample[0], y = sample[1], \
+	           s = 200, linewidth = 3, color = 'black', marker = 'o', facecolors = 'none');
+		ax.scatter(x = sample[0]+0.25, y = sample[1]+0.3, marker='$%d$'%(i), alpha = 1, s=125);
